33db50185a28ce7d5e4defb436c32fe38c449f25
Alex Beregszaszi <alex@rtfs.hu>
2018-04-23 15:04:43 +0100

    Add more denomination tests

---
+++ test/libsolidity/syntaxTests/denominations/denominations.sol
@@ -0,0 +1,9 @@
+contract C {
+	uint constant a = 1 wei + 2 szabo + 3 finney + 4 ether;
+	uint constant b = 1 seconds + 2 minutes + 3 hours + 4 days + 5 weeks + 6 years;
+	uint constant c = 2 szabo / 1 seconds + 3 finney * 3 hours;
+	uint constant d = 4 ether / 3 hours;
+	ufixed constant e = ufixed(4 ether / 3 hours);
+}
+// ----
+// TypeError: (231-248): Type rational_const 10000000000000000 / 27 is not implicitly convertible to expected type uint256. Try converting to type ufixed256x62 or use an explicit conversion.
